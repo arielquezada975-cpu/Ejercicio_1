45f0bf90d6021db71cbd56d5d8712d9f755c7281
MrGato <arielquezada975@gmail.com>
2026-05-12 11:15:43 -0500

    Impresion de arreglo con punteros

--- Ejercicio_1.c
+++ Ejercicio_1.c
@@ -5,15 +5,18 @@ int main()
     int arreglo [] = {2, 4, 6, 8, 10};
     int *ptr; // El puntero apunta al primer elemento del arreglo
 
-    ptr = &arreglo[2]; // El puntero ahora apunta al tercer elemento del arreglo (6)
-    printf("%d\n", *ptr); // Imprime el valor al que apunta el puntero (6)
+    ptr = &arreglo[0]; // Asignamos la dirección del primer elemento del arreglo al puntero
 
-    for (int i = 0; i < 5; i++) {
-        printf("%d ", arreglo[i]);
-        printf("%d ", *ptr); // Imprime los elementos del arreglo utilizando el puntero
+    for (int i = 0; i < 5; i++) 
+    {
+        printf("arreglo[%d] = %d\n", i, *ptr); // Imprime el valor al que apunta el puntero
+        ptr = &arreglo[i];
+        printf("%d\n", *ptr); // Imprime el valor al que apunta el puntero  
+
+        
+        
     }
-    printf("\n");
-    return 0;
+
     
     
 }
