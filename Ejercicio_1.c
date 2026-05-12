f399093e7cc522ff24ed4366800c89d5d4bfb658
MrGato <arielquezada975@gmail.com>
2026-05-12 11:22:20 -0500

    segunda impresión de los punteros

--- Ejercicio_1.c
+++ Ejercicio_1.c
@@ -6,12 +6,13 @@ int main()
     int *ptr; // El puntero apunta al primer elemento del arreglo
 
     ptr = &arreglo[0]; // Asignamos la dirección del primer elemento del arreglo al puntero
-
+// impresion de arreglo con punteros
     for (int i = 0; i < 5; i++) 
     {
-        printf("arreglo[%d] = %d\n", i, *ptr); // Imprime el valor al que apunta el puntero
-        ptr = &arreglo[i];
-        printf("%d\n", *ptr); // Imprime el valor al que apunta el puntero  
+        printf("arreglo[%d] = %d", i, arreglo[i]); // Imprime el valor al que apunta el puntero
+        //ptr = &arreglo[i];
+        printf("%d\n", *(ptr++)); // Imprime el valor al que apunta el puntero  
+
 
         
         
